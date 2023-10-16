ab8472a80b7e151beb815eba9c59396fdc9392f3
PROMAGEAVATAR <115641968+PROMAGEAVATAR@users.noreply.github.com>
2023-10-16 10:31:26 +0530

    Add files via upload

---
+++ ran.py
@@ -0,0 +1,48 @@
+import random 
+
+next=input("enter if you want to roll dice , y for yes , n for no")
+
+if next=='y':
+    #print(random.randint(1,6))
+    number=random.randint(1,6)
+
+    if number==1:
+        print('[-----]')
+        print('[     ]')
+        print('[--0--]')
+        print('[     ]')
+        print('[-----]')
+    if number==2:
+        print('[-----]')
+        print('[ 0   ]')
+        print('[     ]')
+        print('[   0 ]')
+        print('[-----]')
+    if number==3:
+        print('[-----]')
+        print('[     ]')
+        print('[0 0 0]')
+        print('[     ]')
+        print('[-----]')
+    if number==4:
+        print('[-----]')
+        print('[0   0]')
+        print('[     ]')
+        print('[0   0]')
+        print('[-----]')
+    if number==5:
+        print('[-----]')
+        print('[0   0]')
+        print('[  0  ]')
+        print('[0   0]')
+        print('[-----]')
+    if number==6:
+        print('[-----]')
+        print('[0 0 0]')
+        print('[     ]')
+        print('[0 0 0]')
+        print('[-----]')
+    
+
+
+
